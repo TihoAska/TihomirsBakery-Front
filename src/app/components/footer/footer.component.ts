a68ffcf29219d940fb8a0b6cb5d1e43fb0566918
TihoAska <tihomirascic18@gmail.com>
2024-02-22 17:31:10 +0100

    Open add screen after clicking on +

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, HostListener } from '@angular/core';
 import { FooterService } from '../../services/footer.service';
 import { Router } from '@angular/router';
+import { HelperService } from '../../services/helper.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,7 +11,11 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent {
 
-  constructor(public footerService : FooterService, public router : Router) {
+  constructor(
+    public footerService : FooterService,
+    public router : Router, 
+    public helperService : HelperService) 
+  {
     
   }
 
@@ -18,9 +23,8 @@ export class FooterComponent {
     this.footerService.isPlusClicked.next(!this.footerService.isPlusClicked.value);
     if(this.footerService.isPlusClicked.value){
       this.router.navigate(['add']);
-    } 
-    else
-    {
+    } else{
+      this.helperService.windowChoice.next('add');
       this.router.navigate(['']);
     }
   }
