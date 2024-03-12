97843c16ad852535d2e81806f7fafcd0a53df35a
TihoAska <tihomirascic18@gmail.com>
2024-03-12 19:14:40 +0100

    Gym redesign

--- src/app/components/gym-essentials/gym-essentials.component.ts
+++ src/app/components/gym-essentials/gym-essentials.component.ts
@@ -7,14 +7,18 @@ import { Component } from '@angular/core';
 })
 export class GymEssentialsComponent {
 
-    background = { path: '../../../assets/pullup-bar-edited.jpg', name: 'PULLUP-BAR'}
+  background = { path: '../../../assets/pullup-bar-edited.jpg', name: 'PULLUP-BAR'}
 
-    images = [
-      { path: '../../../assets/gym-chalk.jpg', name: 'GYM-CHALK'},
-      { path: '../../../assets/gym-straps.jpg', name: 'GYM-STRAPS'},
-      { path: '../../../assets/chain-belt.png', name: 'GYM-CHALK'},
-      { path: '../../../assets/gym-gloves.jpg', name: 'GYM-STRAPS'},
-      { path: '../../../assets/gym-belt.jpg', name: 'GYM-CHALK'},
-      { path: '../../../assets/resistance-bands.jpg', name: 'GYM-STRAPS'},
-    ]
+  images = [
+    { path: '../../../assets/gym-chalk.jpg', name: 'GYM-CHALK'},
+    { path: '../../../assets/gym-straps.jpg', name: 'GYM-STRAPS'},
+    { path: '../../../assets/chain-belt.png', name: 'GYM-CHALK'},
+    { path: '../../../assets/gym-gloves.jpg', name: 'GYM-STRAPS'},
+    { path: '../../../assets/gym-belt.jpg', name: 'GYM-CHALK'},
+    { path: '../../../assets/resistance-bands.jpg', name: 'GYM-STRAPS'},
+  ]
+
+  ngOnInit(){
+    window.scrollTo(0,0);
+  }
 }
