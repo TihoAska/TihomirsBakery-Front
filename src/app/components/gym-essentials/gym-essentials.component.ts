ed928400eff02506064d80175799b8fadc96f32a
TihoAska <tihomirascic18@gmail.com>
2024-04-02 00:12:48 +0200

    New background image.

--- src/app/components/gym-essentials/gym-essentials.component.ts
+++ src/app/components/gym-essentials/gym-essentials.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class GymEssentialsComponent {
 
-  background = { path: '../../../assets/pullup-bar.jpg', name: 'PULLUP-BAR'}
+  background = { path: '../../../assets/pullup-bar-cartoonish.png', name: 'PULLUP-BAR'}
 
   images = [
     { path: '../../../assets/gym-chalk.jpg', name: 'GYM-CHALK'},
