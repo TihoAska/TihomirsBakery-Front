cfc40aee9ca5b7a3c39fed5b23d78b479ee02203
TihoAska <tihomirascic18@gmail.com>
2024-04-02 00:08:18 +0200

    Deleted unnecessary icons.

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -11,11 +11,6 @@ export class SidebarService {
     { icon: '../../../assets/kitchen.png', name: 'KITCHEN' },
     { icon: '../../../assets/gym-essentials.png', name: 'GYM ESSENTIALS' },
     { icon: '../../../assets/kitchen-essentials.png', name: 'KITCHEN ESSENTIALS' },
-    // { icon: '../../../assets/sandwich-white.png', name: 'SANDWICHES' },
-    // { icon: '../../../assets/croissant-white.png', name: 'SNACKS' },
-    // { icon: '../../../assets/pancake-white.png', name: 'DESSERTS' },
-    // { icon: '../../../assets/coffee-white.png', name: 'HOT BEVERAGE' },
-    // { icon: '../../../assets/shake-white.png', name: 'SHAKES' },
   ];
 
   profilePhoto = { icon: '../../../assets/profile-icon-orange.png', name: 'YOUR PROFILE' } ;
