ba391d7a9d88a5872e598a8195c64fec6ef93bd1
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-01 23:37:27 +0200

    New User methods.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,17 @@
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { User } from './../models/User';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(public jwtHelper : JwtHelperService) { }
+  constructor(
+    public jwtHelper : JwtHelperService,
+    public http : HttpClient) { }
 
 
   public decodeUserFromToken(accessToken : string){
@@ -17,9 +21,17 @@ export class UserService {
       id: tokenPayload.Id,
       email: tokenPayload.Email,
       userName: tokenPayload.UserName,
-      imageURL: tokenPayload.ImageUrl,
+      imageUrl: tokenPayload.ImageUrl,
     }
 
     return user;
   } 
+
+  public getUserById(id : number) : Observable<User>{
+    return this.http.get<User>('https://localhost:7069/api/user/GetById?id=' + id);
+  }
+
+  public updateUser(user : User) : Observable<User>{
+    return this.http.put<User>('https://localhost:7069/api/user/Update', user);
+  }
 }
