1effc1c07c8c33af6914ee44f43b8ad970179d22
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-01 23:33:30 +0200

    Check if user is registering or editing profile.

--- src/app/components/avatar-picker/avatar-picker.component.ts
+++ src/app/components/avatar-picker/avatar-picker.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
 import { HelperService } from '../../services/helper.service';
+import { AccountService } from '../../services/account.service';
+import { BehaviorSubject } from 'rxjs';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-avatar-picker',
@@ -9,19 +12,35 @@ import { HelperService } from '../../services/helper.service';
 })
 export class AvatarPickerComponent {
 
-  selectedAvatarName = this.sidebarService.pickedAvatar.value.name;
-  selectedAvatarIcon = this.sidebarService.pickedAvatar.value.icon;
+  selectedAvatarName = 'avatar';
+  selectedAvatarIcon = (this.accountService.loggedUser.value.id === -1 || this.accountService.loggedUser.value.id === undefined) ? this.sidebarService.pickedAvatar.value.icon : this.accountService.loggedUser.value.imageUrl;
+
 
   constructor(
     public sidebarService : SidebarService,
     public helperService : HelperService,
+    public accountService : AccountService,
+    public userService : UserService
   ) {
+    
+  }
 
+  ngOnInit(){
+    this.accountService.loggedUser.subscribe(res => {
+      if(res.imageUrl){
+        this.sidebarService.$selectedAvatar.next(res.imageUrl);
+      }
+      else{
+        this.sidebarService.$selectedAvatar.next(this.selectedAvatarIcon)
+      }
+    });
   }
 
   closeWindow(){
     this.selectedAvatarName = this.sidebarService.pickedAvatar.value.name;
     this.selectedAvatarIcon = this.sidebarService.pickedAvatar.value.icon;
+
+    this.sidebarService.$selectedAvatar.next(this.selectedAvatarIcon);
 
     this.sidebarService.toggleAvatarPickerWindow.next(false);
     this.helperService.dimBackground.next(false);
@@ -30,6 +49,8 @@ export class AvatarPickerComponent {
   pickAvatar(avatarName, avatarIcon){
     this.selectedAvatarName = avatarName;
     this.selectedAvatarIcon = avatarIcon;
+
+    this.sidebarService.$selectedAvatar.next(this.selectedAvatarIcon);
     
     console.log('avatar icon: ', avatarIcon)
   }
@@ -39,15 +60,35 @@ export class AvatarPickerComponent {
       icon: this.selectedAvatarIcon,
       name: this.selectedAvatarName
     });
-    this.sidebarService.toggleAvatarPickerWindow.next(false);
-    this.helperService.toggleRegisterWindow.next(true);
+
+    this.sidebarService.$selectedAvatar.next(this.selectedAvatarIcon);
+
+    if(this.accountService.loggedUser.value.id != -1){
+      this.accountService.loggedUser.value.imageUrl = this.selectedAvatarIcon;
+      this.sidebarService.toggleAvatarPickerWindow.next(false);
+      this.sidebarService.toggleProfile.next(true);
+
+      this.userService.updateUser(this.accountService.loggedUser.value).subscribe();
+
+    } 
+    else{
+      this.sidebarService.toggleAvatarPickerWindow.next(false);
+      this.helperService.toggleRegisterWindow.next(true);
+    }
   }
 
   goBack(){
     this.selectedAvatarName = this.sidebarService.pickedAvatar.value.name;
     this.selectedAvatarIcon = this.sidebarService.pickedAvatar.value.icon;
 
-    this.sidebarService.toggleAvatarPickerWindow.next(false);
-    this.helperService.toggleRegisterWindow.next(true);
+    this.sidebarService.$selectedAvatar.next(this.selectedAvatarIcon);
+
+    if(this.accountService.loggedUser.value.id != -1){
+      this.sidebarService.toggleAvatarPickerWindow.next(false);  
+      this.sidebarService.toggleProfile.next(true);
+    } else{
+      this.sidebarService.toggleAvatarPickerWindow.next(false);
+      this.helperService.toggleRegisterWindow.next(true);
+    }
   }
 }
