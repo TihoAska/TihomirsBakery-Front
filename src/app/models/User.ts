9aa8447fcc045946baea79cdde4658e8bcd3a345
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-01 23:35:41 +0200

    New constructor for User.

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -4,5 +4,21 @@ export class User{
     firstName ?: string;
     lastName ?: string;
     email ?: string;
-    imageURL ?: string;
+    imageUrl ?: string;
+
+    constructor(
+        id ?: number, 
+        userName ?: string,
+        firstName ?: string,
+        lastName ?: string,
+        email ?: string,
+        imageURL ?: string,
+    ) {
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.imageUrl = imageURL;
+    }
 }
