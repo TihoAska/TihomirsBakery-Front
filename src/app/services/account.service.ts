26a1084b790bb6677b627979ff3131cddf01ed8e
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 19:01:10 +0200

    Check access token.

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -20,4 +20,36 @@ export class AccountService {
   register(registerFormValue) : Observable<HttpEvent<any[]>>{
     return this.http.post<HttpEvent<any[]>>('https://localhost:7069/api/account/Register', registerFormValue);
   }
+
+  checkAccessToken(){
+    const token = localStorage.getItem('accessToken');
+
+    if(this.jwtHelper.isTokenExpired(token)){
+      const refreshToken = localStorage.getItem('refreshToken');
+
+      const refreshRequest : RefreshRequest = {
+        refreshToken: refreshToken,
+      }
+
+      if(refreshToken != null){
+        this.http.post<HttpEvent<any[]>>('https://localhost:7069/api/account/Refresh', refreshRequest).subscribe(res => {
+          localStorage.setItem('accessToken', (<any>res).accessToken);
+        });
+      }
+      else
+      {
+        this.$isFromAuth.next(true);
+        this.helperService.dimBackground.next(true);
+        this.sidebarService.toggleLogin.next(true);
+
+        return false;
+      }
+    } 
+
+    return true;
+  }
+}
+
+export interface RefreshRequest{
+  refreshToken : string,
 }
