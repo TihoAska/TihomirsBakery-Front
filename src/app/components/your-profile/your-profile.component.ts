71cd4db23bf45952fe0e19f045b8676e272b0a6e
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-09 19:02:27 +0200

    Redirect user from your-day page when logged out.

--- src/app/components/your-profile/your-profile.component.ts
+++ src/app/components/your-profile/your-profile.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
 import { AccountService } from '../../services/account.service';
 import { User } from '../../models/User';
+import { Router } from '@angular/router';
+import { NutritionService } from '../../services/nutrition.service';
 
 @Component({
   selector: 'app-your-profile',
@@ -14,7 +16,9 @@ export class YourProfileComponent {
   constructor(
     public sidebarService : SidebarService,
     public accountService : AccountService,
-    public helperService : HelperService) {
+    public helperService : HelperService,
+    public nutritionService : NutritionService,
+    private router : Router) {
     
   }
 
@@ -30,11 +34,47 @@ export class YourProfileComponent {
     this.sidebarService.toggleProfile.next(false);
     this.helperService.dimBackground.next(false);
 
-    this.accountService.loggedUser.next(new User(-1));
+    if(this.router.url == '/your-day'){
+      this.router.navigate(['']);
+    }
+
+    this.emptyValues();
+    
+    this.accountService.$loggedUser.next(new User(-1));
   }
 
   toggleAvatarPicker(){
     this.sidebarService.toggleProfile.next(false);
     this.sidebarService.toggleAvatarPickerWindow.next(true);
+  }
+
+  emptyValues(){
+    this.nutritionService.$totalMealsValue.next({
+      totalCalories: 0,
+      totalFats: 0,
+      totalCarbs: 0,
+      totalProtein: 0,
+    });
+    this.nutritionService.$pickedBreakfastMealsValues.next({
+      calories: 0,
+      carbs: 0,
+      protein: 0,
+      fats: 0,
+    });
+    this.nutritionService.$pickedLunchMealsValues.next({
+      calories: 0,
+      carbs: 0,
+      protein: 0,
+      fats: 0,
+    });
+    this.nutritionService.$pickedDinnerMealsValues.next({
+      calories: 0,
+      carbs: 0,
+      protein: 0,
+      fats: 0,
+    });
+    this.nutritionService.isBreakfastAdded = false;
+    this.nutritionService.isLunchAdded = false;
+    this.nutritionService.isDinnerAdded = false;
   }
 }
