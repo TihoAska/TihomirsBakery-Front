ac98223f50f795574909e4756c64d5bedc54b571
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-05-21 14:59:35 +0200

    Removed unnecessary imports.

--- src/app/guards/userSignedInGuard.ts
+++ src/app/guards/userSignedInGuard.ts
@@ -1,12 +1,10 @@
 import { inject } from "@angular/core";
-import { CanActivateFn, Router } from "@angular/router";
-import { UserService } from "../services/user.service";
+import { CanActivateFn } from "@angular/router";
 import { of } from "rxjs";
 import { AccountService } from "../services/account.service";
 
 export const UserSignedInGuard : CanActivateFn = (route, state) => {
     const accountService = inject(AccountService);
-    const router = inject(Router);
 
     if (accountService.checkAccessToken()) {
         return of(true);
