1e3341da98789978507a96bc44c88eba04c3efe7
DESKTOP-SE6MT31\Tihomir <tihomirascic18@gmail.com>
2024-12-31 22:55:32 +0100

    Adjusted images and videos paths.

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -12,35 +12,35 @@ export class SidebarService {
   private _isRegisterWindowOpen = false;
   private _selectedAvatar = '';
   private _confirmedAvatar = '';
-  private _confirmedAvatarForRegister = '../../../assets/images/avatars/avatar-male-1.png';
-  private _defaultAvatar = '../../../assets/images/avatars/avatar-male-1.png';
+  private _confirmedAvatarForRegister = 'assets/images/avatars/avatar-male-1.png';
+  private _defaultAvatar = 'assets/images/avatars/avatar-male-1.png';
   
 
   avatars = [
-    { icon: '../../../assets/images/avatars/avatar-male-1.png', name: 'avatar-male-1'},
-    { icon: '../../../assets/images/avatars/avatar-male-2.png', name: 'avatar-male-2'},
-    { icon: '../../../assets/images/avatars/avatar-male-3.png', name: 'avatar-male-3'},
-    { icon: '../../../assets/images/avatars/avatar-male-4.png', name: 'avatar-male-4'},
-    { icon: '../../../assets/images/avatars/avatar-male-5.png', name: 'avatar-male-5'},
-    { icon: '../../../assets/images/avatars/avatar-male-6.png', name: 'avatar-male-6'},
-    { icon: '../../../assets/images/avatars/avatar-male-7.png', name: 'avatar-male-7'},
-    { icon: '../../../assets/images/avatars/avatar-male-8.png', name: 'avatar-male-8'},
-    { icon: '../../../assets/images/avatars/avatar-female-1.png', name: 'avatar-female-1'},
-    { icon: '../../../assets/images/avatars/avatar-female-2.png', name: 'avatar-female-2'},
-    { icon: '../../../assets/images/avatars/avatar-female-3.png', name: 'avatar-female-3'},
-    { icon: '../../../assets/images/avatars/avatar-female-4.png', name: 'avatar-female-4'},
-    { icon: '../../../assets/images/avatars/avatar-female-5.png', name: 'avatar-female-5'},
-    { icon: '../../../assets/images/avatars/avatar-female-6.png', name: 'avatar-female-6'},
-    { icon: '../../../assets/images/avatars/avatar-female-7.png', name: 'avatar-female-7'},
+    { icon: 'assets/images/avatars/avatar-male-1.png', name: 'avatar-male-1'},
+    { icon: 'assets/images/avatars/avatar-male-2.png', name: 'avatar-male-2'},
+    { icon: 'assets/images/avatars/avatar-male-3.png', name: 'avatar-male-3'},
+    { icon: 'assets/images/avatars/avatar-male-4.png', name: 'avatar-male-4'},
+    { icon: 'assets/images/avatars/avatar-male-5.png', name: 'avatar-male-5'},
+    { icon: 'assets/images/avatars/avatar-male-6.png', name: 'avatar-male-6'},
+    { icon: 'assets/images/avatars/avatar-male-7.png', name: 'avatar-male-7'},
+    { icon: 'assets/images/avatars/avatar-male-8.png', name: 'avatar-male-8'},
+    { icon: 'assets/images/avatars/avatar-female-1.png', name: 'avatar-female-1'},
+    { icon: 'assets/images/avatars/avatar-female-2.png', name: 'avatar-female-2'},
+    { icon: 'assets/images/avatars/avatar-female-3.png', name: 'avatar-female-3'},
+    { icon: 'assets/images/avatars/avatar-female-4.png', name: 'avatar-female-4'},
+    { icon: 'assets/images/avatars/avatar-female-5.png', name: 'avatar-female-5'},
+    { icon: 'assets/images/avatars/avatar-female-6.png', name: 'avatar-female-6'},
+    { icon: 'assets/images/avatars/avatar-female-7.png', name: 'avatar-female-7'},
   ]
 
   items = [
-    { icon: '../../../assets/images/sidebar-icons/your-day-white.png', name: 'YOUR DAY' },
-    { icon: '../../../assets/images/sidebar-icons/workout-locations.png', name: 'WORKOUT SPOTS' },
-    { icon: '../../../assets/images/sidebar-icons/gym.png', name: 'GYM' },
-    { icon: '../../../assets/images/sidebar-icons/kitchen.png', name: 'KITCHEN' },
-    { icon: '../../../assets/images/sidebar-icons/gym-essentials.png', name: 'GYM ESSENTIALS' },
-    { icon: '../../../assets/images/sidebar-icons/kitchen-essentials.png', name: 'KITCHEN ESSENTIALS' },
+    { icon: 'assets/images/sidebar-icons/your-day-white.png', name: 'YOUR DAY' },
+    { icon: 'assets/images/sidebar-icons/workout-locations.png', name: 'WORKOUT SPOTS' },
+    { icon: 'assets/images/sidebar-icons/gym.png', name: 'GYM' },
+    { icon: 'assets/images/sidebar-icons/kitchen.png', name: 'KITCHEN' },
+    { icon: 'assets/images/sidebar-icons/gym-essentials.png', name: 'GYM ESSENTIALS' },
+    { icon: 'assets/images/sidebar-icons/kitchen-essentials.png', name: 'KITCHEN ESSENTIALS' },
   ];
 
   constructor(private helperService: HelperService, private accountService: AccountService) { }
